ee198db16beb3be063bd381a2ae65a495a2b6480
“Jones <jovicheer.web@gmail.com>
2026-05-08 11:00:28 +0900

    Initial Commit

---
+++ KoreanPhraseBuddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KoreanPhraseBuddy
+//
+//  Created by Jones Wang on 5/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KoreanPhraseBuddy/KoreanPhraseBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoreanPhraseBuddyApp.swift
+//  KoreanPhraseBuddy
+//
+//  Created by Jones Wang on 5/8/26.
+//
+
+import SwiftUI
+
+@main
+struct KoreanPhraseBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
